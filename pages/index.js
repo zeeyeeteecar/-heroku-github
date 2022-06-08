dfca7077fff2dd108ca21b8853eac70a909f2194
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2022-06-08 13:02:05 -0700

    33

--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,29 @@ import ShowData from "./components/ShowData";
 export default function Index() {
   const [dataList, setDataList] = React.useState([]);
 
-  //React.useEffect(() => {
-  axios
-    .all([
-      axios.get("https://api.imgflip.com/get_memes"),
-      //axios.get('https://api.github.com/users/mapbox'),
-      //axios.get('https://api.github.com/users/phantomjs'),
-    ])
-    .then((responseArr) => {
-      //this will be executed only when all requests are complete
-      //console.log('Date created: ', responseArr[0].data.created_at);
-      //console.log('Date created: ', responseArr[1].data.created_at);
-      const fetchedData = responseArr[0].data.data.memes;
-      console.log(fetchedData);
-      setDataList(fetchedData);
-    });
-  //}, []);
+  React.useEffect(() => {
+    axios
+      .all([
+        axios.get("https://api.imgflip.com/get_memes"),
+        //axios.get('https://api.github.com/users/mapbox'),
+        //axios.get('https://api.github.com/users/phantomjs'),
+      ])
+      .then((responseArr) => {
+        //this will be executed only when all requests are complete
+        //console.log('Date created: ', responseArr[0].data.created_at);
+        //console.log('Date created: ', responseArr[1].data.created_at);
+        const fetchedData = responseArr[0].data.data.memes;
+        console.log(fetchedData);
+        setDataList(fetchedData);
+      });
+  }, []);
+
+  const dataListArr = [...dataList];
 
   return (
     <div>
       index, index
-      {<ShowData dataList={dataList} />}
+      {<ShowData dataList={dataListArr} />}
     </div>
   );
 }
