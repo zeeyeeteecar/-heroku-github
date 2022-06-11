fcbe03897fb4646583f12959cca82fd55d73af97
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2022-06-10 23:33:10 -0700

    55

--- pages/components/ShowData.tsx
+++ pages/components/ShowData.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import Image from "next/image";
+import { Container, VStack, Box, IconButton } from "@chakra-ui/react";
+import SearchIcon from "@chakra-ui/icon";
 
-export interface PropsType {
-  dataList: [{ id: string; name: string; url: string }];
-}
+// export interface PropsType {
+//   dataList: [{ id: string; name: string; url: string }];
+// }
 
-export default function ShowData({ dataList }: PropsType) {
+export default function ShowData({ dataList, handle_click_for_pic }) {
+  // export default function ShowData({ dataList }: PropsType) {
   //const ShowData: React.FC<Props> = ({ dataList }) => {
   return (
     <div>
-      {dataList.map((item, index) => {
-        return (
-          <li key={index}>
-            {index} --
-            {item.id} --
-            {item.name} --
-            <img src={item.url} width="300px" height={300} />
-          </li>
-        );
-      })}
+      {dataList &&
+        dataList.map((item, index) => {
+          return (
+            <li key={index + 1}>
+              {index + 1} -{item.id} -{item.name} -
+              <IconButton
+                aria-label="Search database"
+                icon={<SearchIcon />}
+                onClick={(e) => handle_click_for_pic(item.url)}
+              />
+            </li>
+          );
+        })}
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 import axios from "axios";
+import { Container, VStack, Box, Center, SimpleGrid } from "@chakra-ui/react";
+
 import ShowData from "./components/ShowData";
 
-interface Props {
-  dataList: [{ id: string; name: string; url: string }];
-}
+// interface Props {
+//   dataList: [{ id: string; name: string; url: string }];
+// }
 
 export default function Index() {
-  const [dataList, setDataList] = React.useState<
-    [{ id: string; name: string; url: string }]
-  >([{ id: null, name: null, url: null }]);
+  // const [dataList, setDataList] = React.useState<
+  //   [{ id: string; name: string; url: string }]
+  // >([{ id: null, name: null, url: null }]);
+
+  const [dataList, setDataList] = React.useState([]);
+  const [imgUrl, setImgUrl] = React.useState("");
+  const url_Api = "https://api.imgflip.com/get_memes";
 
   React.useEffect(() => {
     axios
       .all([
-        axios.get("https://api.imgflip.com/get_memes"),
+        axios.get(url_Api),
         //axios.get('https://api.github.com/users/mapbox'),
         //axios.get('https://api.github.com/users/phantomjs'),
       ])
@@ -23,16 +29,35 @@ export default function Index() {
         //console.log('Date created: ', responseArr[0].data.created_at);
         //console.log('Date created: ', responseArr[1].data.created_at);
         const fetchedData = responseArr[0].data.data.memes;
-        console.log(fetchedData);
+        //console.log(fetchedData);
         setDataList(fetchedData);
       });
   }, []);
 
+  function handle_click_for_pic(e) {
+    console.log(e);
+    setImgUrl(e);
+  }
+
   return (
-    <div>
-      index, index, index
-      {<ShowData dataList={dataList} />}
-    </div>
+    <VStack borderWidth={5}>
+      <Center borderWidth={5} width={1000} h="90vh">
+        <SimpleGrid columns={2} spacing={5} padding="5px">
+          <Box height="80vh" overflowY="scroll" padding="5px" margin={5}>
+            {" "}
+            {
+              <ShowData
+                dataList={dataList}
+                handle_click_for_pic={handle_click_for_pic}
+              />
+            }
+          </Box>
+          <Box height="80vh" padding="5px" margin={5}>
+            <img src={imgUrl} width="300px" height={300} />
+          </Box>
+        </SimpleGrid>
+      </Center>
+    </VStack>
   );
 }
 
